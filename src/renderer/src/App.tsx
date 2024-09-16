e6f750fc51776140dda2f3ac4cb19e5850f3be58
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-09-16 12:39:49 -0300

    First commit of markdown editor with electron

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,11 @@
+function App(): JSX.Element {
+  return (
+    <>
+      <h1>Hi Guys!</h1>
+      <p>Here is a paragraph of Electron for to show on screen</p>
+      <button>Click Me</button>
+    </>
+  )
+}
+
+export default App
